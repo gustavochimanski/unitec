ee334814283112441b1ac07bde47967b4314c04b
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-05-06 17:02:18 -0300

    corrigindo nomes de arquivos com erro de digitação

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
-import Hero from "../Components/Hero"
-import ProblemsSection from "../Components/ProblemsSection"
-import ComoFazemosDiferenca from "../Components/ComoFazemosDiferença"
-import FuncionalidadesPoderosas from "../Components/FuncionalidadesPoderosas"
-import TestimonialsSection from "../Components/TestmonialSection"
-import NumerosQualidade from "../Components/NumerosQualidade"
-import SuportePersonalizado from "../Components/SuportPersonalizado"
-import SoliciteDemo from "../Components/SoliciteDemo"
-import Footer from "../Components/Footer"
+import Hero from "../Components/Hero";
+import ProblemsSection from "../Components/ProblemsSection";
+import ComoFazemosDiferenca from "../Components/ComoFazemosDiferença";
+import FuncionalidadesPoderosas from "../Components/FuncionalidadesPoderosas";
+import TestimonialsSection from "../Components/TestimonialsSection"; // <-- CORRIGIDO
+import NumerosQualidade from "../Components/NumerosQualidade";
+import SuportePersonalizado from "../Components/SuportePersonalizado"; // <-- CORRIGIDO
+import SoliciteDemo from "../Components/SoliciteDemo";
+import Footer from "../Components/Footer";
+
 
 function Home() {
   return (
