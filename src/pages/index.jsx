0aac60de6680041a2397e61319e54f5929550e63
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-05-06 17:05:25 -0300

    ...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,12 @@
-import Hero from "@/Components/Hero";
-import ProblemsSection from "../Components/ProblemsSection";
-import ComoFazemosDiferenca from "../Components/ComoFazemosDiferença";
-import FuncionalidadesPoderosas from "../Components/FuncionalidadesPoderosas";
-import TestimonialsSection from "../Components/TestimonialsSection"; // <-- CORRIGIDO
-import NumerosQualidade from "../Components/NumerosQualidade";
-import SuportePersonalizado from "../Components/SuportePersonalizado"; // <-- CORRIGIDO
-import SoliciteDemo from "../Components/SoliciteDemo";
-import Footer from "../Components/Footer";
+import Hero from "/";
+import ProblemsSection from "./Components/ProblemsSection";
+import ComoFazemosDiferenca from "./Components/ComoFazemosDiferença";
+import FuncionalidadesPoderosas from "./Components/FuncionalidadesPoderosas";
+import TestimonialsSection from "./Components/TestimonialsSection"; // <-- CORRIGIDO
+import NumerosQualidade from "./Components/NumerosQualidade";
+import SuportePersonalizado from "./Components/SuportePersonalizado"; // <-- CORRIGIDO
+import SoliciteDemo from "./Components/SoliciteDemo";
+import Footer from "./Components/Footer";
 
 
 function Home() {
