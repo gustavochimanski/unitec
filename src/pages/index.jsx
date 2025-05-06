4efeb6d6f3531affee6c113f9581f0bc73bd1a0f
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-05-06 16:55:46 -0300

    ...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,28 @@
-// src/pages/index.jsx
-import Home from "./Home"
+import Hero from "../Components/Hero"
+import ProblemsSection from "../Components/ProblemsSection"
+import ComoFazemosDiferenca from "../Components/ComoFazemosDiferença"
+import FuncionalidadesPoderosas from "../Components/FuncionalidadesPoderosas"
+import TestimonialsSection from "../Components/TestmonialSection"
+import NumerosQualidade from "../Components/NumerosQualidade"
+import SuportePersonalizado from "../Components/SuportPersonalizado"
+import SoliciteDemo from "../Components/SoliciteDemo"
+import Footer from "../Components/Footer"
 
-export default function Index() {
-  return <Home />
+function Home() {
+  return (
+    <>
+      <Hero/>
+      <ProblemsSection />
+      <ComoFazemosDiferenca />
+      <FuncionalidadesPoderosas />
+      <TestimonialsSection />
+      <NumerosQualidade />
+      <SuportePersonalizado />
+      <SoliciteDemo />
+      <Footer />
+    </>
+  )
 }
+
+export default Home
 
